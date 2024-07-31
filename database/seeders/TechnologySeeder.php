da654872f3cd901c0307f063528baff8b10609d8
GencoMattia <gencomattia@hotmail.it>
2024-07-31 17:20:29 +0200

    seeded technology table

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,28 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologiesData = [
+            ['name' => 'HTML'],
+            ['name' => 'CSS'],
+            ['name' => 'JavaScript'],
+            ['name' => 'PHP'],
+            ['name' => 'Laravel'],
+            ['name' => 'Vue.js'],
+            ['name' => 'React'],
+            ['name' => 'Node.js'],
+            ['name' => 'MySQL'],
+            ['name' => 'PostgreSQL'],
+            ['name' => 'Docker'],
+            ['name' => 'Kubernetes'],
+            ['name' => 'Git'],
+            ['name' => 'Webpack'],
+            ['name' => 'SASS'],
+        ];
+
+        foreach($technologiesData as $technologyData){
+            $technology = new Technology();
+            $technology->name = $technologyData["name"];
+            $technology->save();
+        }
     }
 }
